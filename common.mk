34221337341107de6960ade8cf10465c34dea5bb
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-09-14 22:18:06 +0300

    Revert "guacamoleb: Switch to prebuilt liba2dpoffload"
    This reverts commit 058ef298158f4e1ceb88891a7e425de07524a88a.
    
    Change-Id: I52fd2287a358ae6fb9a847d3f2aac8083776a41c

--- common.mk
+++ common.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
     libbthost_if \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
